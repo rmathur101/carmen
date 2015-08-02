bcad515267f53300d46041f298d2d2243896d6f8
Rohan Mathur <rmathur101@gmail.com>
2015-08-02 17:44:31 -0500

    sign ups

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -8,19 +8,12 @@ class Api::CarsController < AuthController
 
 	def create
 		#need some way to put in checks that all of this information has been POSTED
-		puts params[:model]
-		puts params[:year]
-		puts params[:color]
-		puts params[:license]
-
 		@user.resident.cars << Car.create(car_params)
-		resident = @user.resident
-		@cars = resident.cars
+		@cars = @user.resident.cars
 	end
 
 	private
 
-	#are we always guaranteed a :car? investigate.
 	def car_params
 		params.permit(:model, :year, :color, :license)
 	end
--- app/controllers/api/sign_ups_controller.rb
+++ app/controllers/api/sign_ups_controller.rb
@@ -1,10 +1,63 @@
-class Api::SignUpsController < ApplicationController
+class Api::SignUpsController < AuthController
+
 	def index
-		#TODO: authenticate and retrieve user with token
-		@resident = Resident.first
+		@resident = @user.resident
 	end
 
 	def create
-
+		print params
+		# User.update(@user.id, first_name: params[''])
+		@resident = @user.resident
 	end
+
+	private
+
+	def sign_up_params
+		params.permit(
+			:first_name,
+			:last_name,
+			:street,
+			:unit_number,
+			:state,
+			:city,
+			:zip,
+			:phone_number,
+			:email,
+			:confirm_email
+			:password,
+			:confirm_password
+			)
+	end
+
 end
+
+
+# Headers
+# Authorization
+
+# Input Parameters
+# first_name
+# last_name
+# street
+# unit_number
+# state
+# city
+# zip
+# phone_number
+# email
+# confirm_email
+# password TODO
+# confirm_password TODO
+# profile_image (optional) TODO
+
+# Return Values (updated profile information)
+# first_name
+# last_name
+# street
+# unit_number
+# state
+# city
+# zip
+# phone_number
+# email
+# profile_image_url
