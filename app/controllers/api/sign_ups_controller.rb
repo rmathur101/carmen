9dbf6987baaad1c32ca3392faa7848bd7bc9e954
Rohan Mathur <rmathur101@gmail.com>
2015-08-02 18:08:07 -0500

    added to create function in sign-ups controller

--- app/controllers/api/sign_ups_controller.rb
+++ app/controllers/api/sign_ups_controller.rb
@@ -5,9 +5,20 @@ class Api::SignUpsController < AuthController
 	end
 
 	def create
-		print params
-		# User.update(@user.id, first_name: params[''])
-		@resident = @user.resident
+		User.update(@user.id,
+			first_name: sign_up_params[:first_name],
+			last_name: sign_up_params[:last_name]
+		)
+		Unit.update(@user.resident.unit.id,
+			number: sign_up_params[:unit_number]
+		)
+		Address.update(@user.resident.address.id,
+			street: sign_up_params[:street],
+			state: sign_up_params[:state],
+			city: sign_up_params[:city],
+			zip: sign_up_params[:zip]
+		)
+		return @resident = User.find(@user.id).resident
 	end
 
 	private
@@ -23,41 +34,10 @@ class Api::SignUpsController < AuthController
 			:zip,
 			:phone_number,
 			:email,
-			:confirm_email
+			:confirm_email,
 			:password,
 			:confirm_password
-			)
+		)
 	end
 
 end
-
-
-# Headers
-# Authorization
-
-# Input Parameters
-# first_name
-# last_name
-# street
-# unit_number
-# state
-# city
-# zip
-# phone_number
-# email
-# confirm_email
-# password TODO
-# confirm_password TODO
-# profile_image (optional) TODO
-
-# Return Values (updated profile information)
-# first_name
-# last_name
-# street
-# unit_number
-# state
-# city
-# zip
-# phone_number
-# email
-# profile_image_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 	has_one :api_key
 	has_one :resident
 
+
 end
