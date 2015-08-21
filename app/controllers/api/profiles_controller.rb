21ccba6bd68eca3b3fb441fd35e318b1c9a7becb
Britney Lyons <blyons333@gmail.com>
2015-08-20 21:46:08 -0500

    Finished edit profile and fixed some naming of get profile to match what we have in the documentation

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -4,5 +4,17 @@ class Api::ProfilesController < AuthController
   end
 
   def edit
+  	@user.update(first_name: update_profile_params[:first_name],
+  		last_name: update_profile_params[:last_name],
+  		email: update_profile_params[:email]
+  	)
+  	
+  	#TODO: Update to handle multiple phone numbers. Currently, they can only have one phone number
+  	@user.phones.first.update(number: update_profile_params[:phone_number])
+  	@resident = @user.resident
+  end
+
+  def update_profile_params
+	return params.permit(:first_name, :last_name, :phone_number, :email)
   end
 end
