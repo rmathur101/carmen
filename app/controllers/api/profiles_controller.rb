d88ea2c95f9fc60c389f1e869fbfc20540da6604
Britney Lyons <blyons333@gmail.com>
2015-09-27 21:55:51 -0500

    Finished error handling for the edit profile route

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -4,14 +4,33 @@ class Api::ProfilesController < AuthController
   end
 
   def edit
-  	@user.update(first_name: update_profile_params[:first_name],
-  		last_name: update_profile_params[:last_name],
-  		email: update_profile_params[:email]
+    verify_edit_params
+    verify_edit_param_values
+
+  	@user.update(first_name: params[:first_name],
+            		 last_name: params[:last_name],
+            		 email: params[:email]
   	)
   	
   	#TODO: Update to handle multiple phone numbers. Currently, they can only have one phone number
-  	@user.phones.first.update(number: update_profile_params[:phone_number])
+  	@user.phones.first.update(number: params[:phone_number])
   	@resident = @user.resident
+  end
+
+  private
+  def verify_edit_params
+    verify_param_exists(:first_name)
+    verify_param_exists(:last_name)
+    verify_param_exists(:phone_number)
+    verify_param_exists(:email)
+    verify_param_exists(:confirm_email)
+  end
+
+  def verify_edit_param_values
+    User.validate_name(params[:first_name], "first name")
+    User.validate_name(params[:last_name], "last name")
+    User.validate_phone(params[:phone_number])
+    User.validate_email(params[:email], params[:confirm_email])
   end
 
   def update_profile_params
